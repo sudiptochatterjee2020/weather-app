36d4e2fa22ce7d619f63aaac4e4103606f06b7e4
Sudipto Chatterjee <sudiptochatterjee2020@gmail.com>
2020-07-30 18:29:00 +0530

    small changes to src/app.js to display messages

--- public/js/app.js
+++ public/js/app.js
@@ -7,12 +7,16 @@ const searchString = document.querySelector('input');
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const loc = searchString.value
+    msgone.textContent = "Loading..."
+    msgtwo.textContent = ""
     fetch(weatherUrl + loc).then((response) => {
         response.json().then((data) => {
             if (data.error) {
+                msgone.textContent = ''
                 msgtwo.textContent = data.error
             } else {
                 if (data.errorMessage) {
+                    msgone.textContent = ''
                     msgtwo.textContent = data.errorMessage    
                 } else {
                     msgone.textContent = 'The current temperature is ' + data.currently + ' Celsius. It feels like ' + data.feelsLike + ' Celsius.'
